d494f0ecb2cf871b6ba1f73d8b20b9138a91056b
Dhanush CK <dhanushck01@gmail.com>
2024-09-26 23:23:44 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import React, { useRef, useState } from "react";
 
 function App() {
+  const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="top-nav">
+        <h1>Music Player</h1>
+        <audio ref={audioRef} src="../public/song/memory_reboot_-_vøj,_narvent_[edit_audio](256k).mp3"></audio>
+        <button onClick={handlePlayPause}>
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+      </div>
     </div>
   );
 }
